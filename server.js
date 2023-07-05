ca493c6332cba4a20b112a4d355b4ae96c129cb6
Chinmay <chinmaykamble@gmail.com>
2023-07-05 15:03:47 +0530

    architecture formed

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const commentRoutes = require("./routes/comments/comment");
+const postRoutes = require("./routes/posts/posts");
+const userRoutes = require("./routes/users/users");
+
+require("./config/dbConnect");
+
+const app = express();
+
+//middlewares
+
+//users route
+app.use("/api/v1/users", userRoutes);
+
+//posts route
+app.use("/api/v1/posts", postRoutes);
+
+//comments
+app.use("/api/v1/comments", commentRoutes);
+
+//Error handler middlewares
+//listen server
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, console.log(`Servver is running on PORT ${PORT}`));
