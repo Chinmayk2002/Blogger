05357fb70b9048a25a277a254b66ad56dc79c61b
Chinmay <chinmaykamble@gmail.com>
2023-07-06 13:58:08 +0530

    added error handler and reg/login logic

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const commentRoutes = require("./routes/comments/comment");
 const postRoutes = require("./routes/posts/posts");
 const userRoutes = require("./routes/users/users");
-
+const globalErrHandler = require("./middlewares/globalHandler");
 require("./config/dbConnect");
 
 const app = express();
 
 //middlewares
-
+app.use(express.json()); // passing incomming data
 //users route
 app.use("/api/v1/users", userRoutes);
 
@@ -20,6 +20,7 @@ app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/comments", commentRoutes);
 
 //Error handler middlewares
+app.use(globalErrHandler);
 //listen server
 const PORT = process.env.PORT || 9000;
-app.listen(PORT, console.log(`Servver is running on PORT ${PORT}`));
+app.listen(PORT, console.log(`Server is running on PORT ${PORT}`));
